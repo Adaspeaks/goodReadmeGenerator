e9594e7ab04a9fe468c2a4a6f0edf04332bf559a
Adaspeaks <adaspeaks@gmail.com>
2021-07-31 07:44:19 -0700

    Create a function to initialize app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("fs")
-const inquirer = require("inquirer")
-const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -8,8 +8,8 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    generator(inquirer);
+}
 
-// Function call to initialize app
 init();
