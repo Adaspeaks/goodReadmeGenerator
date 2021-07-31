3b0fd1a9cc4c7f52fa5d4885a4194d658a1fa4c2
Adaspeaks <adaspeaks@gmail.com>
2021-07-31 07:37:39 -0700

    got rid of todos

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(data) {
   if (data.badge == "MIT"){
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
@@ -14,7 +12,6 @@ function renderLicenseBadge(data) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
