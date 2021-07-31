972372b35b790ceed412a0e61d2176194ac26795
Adaspeaks <adaspeaks@gmail.com>
2021-07-31 05:44:52 -0700

    incorperated outine made data uniform

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license.badge == "MIT"){
+function renderLicenseBadge(data) {
+  if (data.badge == "MIT"){
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  }else if (license.badge == "MPL-2.0"){
+  }else if (data.badge == "MPL-2.0"){
     return '[![License: MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-red.svg)](https://opensource.org/licenses/MPL-2.0)'
-  } else if (license.badge == "GNU GPLv3"){
+  } else if (data.badge == "GNU GPLv3"){
     return '[![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/gpl-3.0.html)'
-  }else if (license.badge == "Unlicense"){
+  }else if (data.badge == "Unlicense"){
     return '[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-lightgrey.svg)](https://opensource.org/licenses/unlicense)'
   }else {
     return ''
@@ -19,21 +19,22 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
-  ${renderLicenseBadge(license.badge)}
+  ${renderLicenseBadge(data.badge)}
+
+  ## Description
+
+  ## Installation
+
+  ## Usage
+
+  ## Contribution Guidlines
+
+  ## Test Instructions
+
+  ## Contant Me
+
+
 `;
 }
 
 module.exports = generateMarkdown;
-
-
-// must include a sample READme
-// table of contents 
-
-// title
-// description 
-// installation 
-// usage information 
-// contribution guidlines
-// test instructions
-// github username with link functionality
-// email address 
