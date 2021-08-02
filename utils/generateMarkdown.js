0f51d1a7ed88800d2b97ceaacee8cdaa6ccb724f
Adaspeaks <adaspeaks@gmail.com>
2021-08-02 06:15:28 -0700

    test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 function renderLicenseBadge(data) {
-  if (data.badge == "MIT"){
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  }else if (data.badge == "MPL-2.0"){
-    return '[![License: MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-red.svg)](https://opensource.org/licenses/MPL-2.0)'
-  } else if (data.badge == "GNU GPLv3"){
-    return '[![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/gpl-3.0.html)'
-  }else if (data.badge == "Unlicense"){
-    return '[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-lightgrey.svg)](https://opensource.org/licenses/unlicense)'
-  }else {
-    return ''
+  if (data.badge == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (data.badge == "MPL-2.0") {
+    return "[![License: MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-red.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (data.badge == "GNU GPLv3") {
+    return "[![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/gpl-3.0.html)";
+  } else if (data.badge == "Unlicense") {
+    return "[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-lightgrey.svg)](https://opensource.org/licenses/unlicense)";
+  } else {
+    return "";
   }
 }
 
@@ -18,18 +18,19 @@ function generateMarkdown(data) {
 
   ${renderLicenseBadge(data.badge)}
 
-  ## Description
+## Description
 
   ${data.description}
 
-  ## Table of Contents
+## Table of Contents
 
-  * [How to install](#Installation)
-  * [How to use](#Usage)
-  * [How to Contribute] (#Contribution Guidelines)
-  * [Testing Instructions](#Testing)
-  * [Contact Info](#Contacts)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribution](#Contribution)
+  * [Testing](#Testing)
+  * [Contacts](#Contacts)
 
+<a name="Installation"></a>
 ## Installation
 
   To install the dependencies for the application use the following:
@@ -38,26 +39,30 @@ function generateMarkdown(data) {
   ${data.install}
   \`\`\`
 
+<a name="Usage"></a>
 ## Usage
 
   \`\`\`
   ${data.usage}
   \`\`\`
 
+<a name="Contribution"></a>
 ## Contribution
 
   \`\`\`
   ${data.contribute}
   \`\`\`
 
+<a name="Testing"></a>
 ## Testing
+
   To test the application:
 
   \`\`\`
   ${data.test}
   \`\`\`
 
-
+<a name="contacts"></a>
 # Contacts
 
   You can find my other repositories at: https://github.com/${data.username}
